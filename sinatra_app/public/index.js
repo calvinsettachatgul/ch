d067e8638d256866d858a8c3efec4cd2f42c7fa5
Henry V <hvega.dev@gmail.com>
2018-04-29 18:32:22 -0700

    sinatra and ajax add

---
+++ sinatra_app/public/index.js
@@ -0,0 +1,30 @@
+window.onload = init;
+
+function init() {
+  console.log("JavaScript is ready.");
+  var button = $('#json');
+  console.log(button);
+  button.click(getJson);
+};
+
+function getJson() {
+  console.log("Got to JSON");
+  var $request = $.ajax(
+    {
+      dataType: 'json',
+      type: 'GET',
+      url: 'http://localhost:4567/json'
+    }
+  );
+
+  $request.then(
+    function (response) {
+      console.log(response);
+    },
+    function (response) {
+      console.log(response);
+    }
+
+  )
+};
+
