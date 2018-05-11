02bafdf6641808be5f256930ee7cb65acd8d8bbb
Calvin Settachatgul <calvin.settachatgul@autodesk.com>
2018-05-10 20:46:58 -0700

    converting to boolean

---
+++ rithm_intro_javascript/variables_primitives.js
@@ -0,0 +1,42 @@
+/*
+variables and primitives
+variables
+
+adding two strings together
+
+var
+
+prompt function
+
+comments
+
+primitive types
+
+string number boolean undefined
+
+type of 
+
+double not 
+
+changing type
+
+
+!! convert to boolean
+
+typeof
+
+toString();
+
+parseInt();
+
+parseFloat();
+
+Number();
+
+
+ */
+
+
+console.log("converting to boolean");
+
+console.log("!!\"hello\"", !!"hello");
