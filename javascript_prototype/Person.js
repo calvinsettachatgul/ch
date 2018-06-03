b769fef145f4133bc9d516e8d92a8c697a212d15
Henry V <hvega.dev@gmail.com>
2018-06-03 11:12:57 -0700

    add Person methods and requirements

--- javascript_prototype/Person.js
+++ javascript_prototype/Person.js
@@ -1,10 +1,17 @@
 // let's work with an Options hash
 
+let PersonMethods = require('./PersonMethods');
+console.log(PersonMethods);
+PersonMethods.hello();
+
 let Person = function(opts){
   this.firstName = opts.firstName;
   this.lastName = opts.lastName;
   this.email = opts.email;
 }
+
+Person.prototype.hello = PersonMethods.hello;
+Person.prototype.goodbye = PersonMethods.goodbye;
 
 //driver code
 let henryOpts = {
@@ -24,3 +31,6 @@ let cal = new Person(calvinOpts);
 
 console.log(hen.firstName === "Henry");
 console.log(cal.firstName === "Calvin");
+
+console.log(hen.hello() === "Hello, this is me.");
+console.log(hen.goodbye() === "Goodbye from me, Henry");
