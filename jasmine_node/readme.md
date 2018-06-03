28b35a93ea64a9189119123e4260d6a1b8df94dd
Calvin Settachatgul <calvin.settachatgul@autodesk.com>
2018-06-02 19:15:28 -0700

    Add orange tree

--- jasmine_node/readme.md
+++ jasmine_node/readme.md
@@ -1,4 +1,3 @@
-
 https://jasmine.github.io/pages/getting_started.html
 
 working with jasmine locally with node
--- jasmine_node/spec/jasmine_examples/OrangeTreeSpec.js
+++ jasmine_node/spec/jasmine_examples/OrangeTreeSpec.js
@@ -1,8 +1,7 @@
 describe("OrangeTree", function(){
 	var OrangeTree = require('../../lib/jasmine_examples/OrangeTree');
 	it("has a height", function(){
-		console.log("running orange tree spec")
 		let testOrangeTree = new OrangeTree({"height": 1});
 		expect(testOrangeTree.height).toBe(1);
 	});
-})
+});
