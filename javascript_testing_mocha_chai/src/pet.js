04b83ad3a4c6ee993a0595845eb756d6f8b2d227
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-07-20 18:57:36 +0000

    changes to pet testing

--- javascript_testing_mocha_chai/src/pet.js
+++ javascript_testing_mocha_chai/src/pet.js
@@ -4,29 +4,18 @@ let Pet = function(name, breed, age){
   this.age = age;
 }
 
-Pet.prototype.toHTML = function(){
-  let result = '<h1>' + this.name + '</h1>'
-  result += '<ul>'
-  let petKeys = Object.keys(this)
-  for( let i = 0; i < petKeys.length; i++){
-    result += '<li>' + this[petKeys[i]] + '</li>';
-  }
-  result += '</ul>';
-  return result;
+Pet.prototype.speak = function(){
+  return "bark bark!";
 }
 
 Pet.prototype.toString = function(){
   return this.name.toString();
 }
 
-Pet.prototype.speak = function(){
-  return "bark bark!";
-}
-
-let Rover = new Pet("Rover", "Golden Retriever", 2);
-let Fido = new Pet("Fido", "Chihuahua", 5)
-console.log(Rover.age === 2)
-console.log(Fido.age === 5)
-console.log(Rover.speak() === "bark bark!");
+// let Rover = new Pet("Rover", "Golden Retriever", 2);
+// let Fido = new Pet("Fido", "Chihuahua", 5)
+// console.log(Rover.age === 2)
+// console.log(Fido.age === 5)
+// console.log(Rover.speak() === "bark bark!");
 
 module.exports = Pet;
