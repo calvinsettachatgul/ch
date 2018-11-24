8d7ea7675c816d41ab124be39bd757f6bd1543a2
Henry Vega <hvega.dev@gmail.com>
2018-11-24 16:58:29 +0000

    feature-8 finished implementation of isPrime tests working

--- algorithms/is_prime.js
+++ algorithms/is_prime.js
@@ -7,3 +7,23 @@ So, i will run a while loop and increase by 1.
 (look at the code example. If you dont get it. this is not your cake.
 do learn javaScript basics and come back.)
 */
+
+function isPrime (num) {
+    let questionableFactor = 2;
+    let result = true
+    if(num === 2){
+      result = true
+    }
+    else{
+        for( let i = questionableFactor; i < num; i++){
+            if(num % questionableFactor === 0){
+                result = false;
+            }
+        }
+    }
+    return result
+}
+
+module.exports = isPrime;
+
+// this will be O(N) because we have one for loop
