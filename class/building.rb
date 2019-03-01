89631bb928dce272a896be77f25482bd6b2e4de6
EC2 Default User <ec2-user@ip-172-31-16-148.ec2.internal>
2019-03-01 20:26:24 +0000

    feature-28: remove extra class

--- class/building.rb
+++ class/building.rb
@@ -43,5 +43,71 @@ restaurant.color = 'blue'
 puts restaurant.color
 puts restaurant.type
 
-# class Restaurant < Building
-# end
+
+class Building
+  attr_reader :size, :color
+  attr_writer :size, :color
+
+  def initialize(size, color)
+    @size = size
+    @color = color
+  end
+
+  def come_in
+    puts "Come on in!"
+  end
+
+  def size
+    @size
+  end
+
+  def color
+    @color
+  end
+
+end
+
+class Room < Building
+
+  def initialize(people,floor,room_number, number_of_doors, window_number, ac)
+    @people = people
+    @floor = floor
+    @room_number = room_number
+    @number_of_doors =  number_of_doors
+    @window_number = window_number 
+    @ac = ac
+  end
+
+  def people=(new_people)
+    @people = new_people
+  end
+
+  def floor
+    @floor
+  end
+
+  def room_number
+    @room_number
+  end
+
+  def number_of_doors
+    @number_of_doors
+  end
+
+  def window_number
+    @window_number
+  end
+
+  def ac
+    @ac
+  end
+
+end
+
+bedroom = Room.new(2, 4, 52, 2, 2, "none")
+bedroom.come_in
+p bedroom
+bedroom.people = 1
+p bedroom
+p bedroom.ac
+p bedroom
